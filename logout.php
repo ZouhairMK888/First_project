c86d44bd9468f3249f993432f46bfc30b30ff7b2
Zouhair <zouhairmk888@gmail.com>
2024-12-10 11:01:36 +0100

    Initial commit

---
+++ logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+        function preventBack() {
+            window.history.forward();
+        }
+        setTimeout(preventBack, 0);
+        window.onunload = function () {
+            null;
+        };
+    </script>
+</head>
+<body>
+    <script type="text/javascript">
+        setTimeout(function () {
+            window.location.href = 'index.php';
+        }, 0); 
+    </script>
+</body>
+</html>
